eba61ce647975b4bf1eeaed2b1b6ad40accf7d34
Jason Kiraly <jaykiraly@gmail.com>
2019-01-18 15:21:22 -0800

    created basic routes.

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("GET")
+
+	log.Println("Listening on Port 3333...")
+	log.Fatal(http.ListenAndServe(":3333", router))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("signup invoked")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("login invoked")
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("signup invoked")
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	fmt.Println("TokenVerify invoked")
+	return nil
 }
